a695ac619bcb26a8bd742a2e68a8d4143d3fcef8
Akihito Koriyama <akihito.koriyama@gmail.com>
2015-05-19 22:57:49 +0900

    should period at the end.

--- src/Annotation/CookieParam.php
+++ src/Annotation/CookieParam.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * This file is part of the Ray.WebContextParam
+ * This file is part of the Ray.WebContextParam.
  *
  * @license http://opensource.org/licenses/bsd-license.php BSD
  */
