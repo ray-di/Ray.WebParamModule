51ccdfa20b7436418159e6f706fee2289cbe3fc8
Akihito Koriyama <akihito.koriyama@gmail.com>
2017-06-02 10:29:45 +0900

    fist commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# Ray.WebParamModule
+
+This is web context parameter annotations package for [BEAR.Resource](https://github.com/bearsunday/BEAR.Resource).
+
---
+++ src/Annotation/AbstractWebContextParam.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * This file is part of the BEAR.Resource package.
+ *
+ * @license http://opensource.org/licenses/MIT MIT
+ */
+namespace Ray\WebContextParam\Annotation;
+
+abstract class AbstractWebContextParam
+{
+    /**
+     * Key of query parameter
+     *
+     * @var string
+     */
+    public $key;
+
+    /**
+     * Parameter(Variable) name
+     *
+     * @var string
+     */
+    public $param;
+}
