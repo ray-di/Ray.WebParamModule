c6d26d1cd53f6226b26ec34e9fc05c47563f457a
Akihito Koriyama <akihito.koriyama@gmail.com>
2021-01-10 16:52:48 +0900

    Add attributes

--- src/Annotation/AbstractWebContextParam.php
+++ src/Annotation/AbstractWebContextParam.php
@@ -1,9 +1,5 @@
 <?php
-/**
- * This file is part of the BEAR.Resource package.
- *
- * @license http://opensource.org/licenses/MIT MIT
- */
+
 namespace Ray\WebContextParam\Annotation;
 
 abstract class AbstractWebContextParam
