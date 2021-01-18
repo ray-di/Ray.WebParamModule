4d38dc7d7f19d3c0147ade05a38d3fb4d8164660
Akihito Koriyama <akihito.koriyama@gmail.com>
2021-01-18 12:21:56 +0900

    Make IS_REPEATABLE and be applicable to methods

--- src/Annotation/FilesParam.php
+++ src/Annotation/FilesParam.php
@@ -8,7 +8,7 @@ use Attribute;
  * @Annotation
  * @Target("METHOD")
  */
-#[Attribute(Attribute::TARGET_PARAMETER)]
+#[Attribute(Attribute::TARGET_PARAMETER | Attribute::TARGET_METHOD | Attribute::IS_REPEATABLE)]
 final class FilesParam extends AbstractWebContextParam
 {
     const GLOBAL_KEY = '_FILES';
